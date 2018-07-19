b3ad3c10700e1481971112e7693f8d0fe72d2027
saragai <saragai_t@outlook.jp>
2018-07-20 03:00:00 +0900

    [implement] visualization and entropy score

--- source/k_means.py
+++ source/k_means.py
@@ -2,6 +2,10 @@
 import os
 import os.path
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import pickle
 from PIL import Image
 from load.load_mnist import load
 
@@ -19,10 +23,6 @@ def m_step(data, masks):
     return centers
 
 
-def accuracy(labels, masks):
-    print(np.array([[np.sum(labels[mask] == label) for label in range(10)] for mask in masks]))
-
-
 def entropy(array):
     if array.ndim == 1:
         array.shape = (1, array.size)
@@ -32,56 +32,116 @@ def entropy(array):
     return -np.sum(p * log_p, axis=1)
 
 
-def entropy_score(result):
+def entropy_score(data, labels, centers):
+    masks = e_step(data, centers)
+    result = np.array([
+        [np.sum(labels[mask] == label)/np.sum(labels == label) for label in range(10)]
+        for mask in masks])
     weight = result.sum(axis=1)/result.sum()
+
     return np.sum(weight * entropy(result))
 
 
-def draw_heat_map(result):
-    pass
+def draw_heatmap(data, labels, centers, save_dir="", seed=1):
+    masks = e_step(data, centers)
+    raw_result = np.array([
+        [np.sum(labels[mask] == label)/np.sum(labels == label) for label in range(10)]
+        for mask in masks])
+
+    # sort for figure
+    result = np.zeros(raw_result.shape)
+    for label in range(10):
+        idx = np.argmax(raw_result[:, label])
+        result[label] = raw_result[idx]
+        raw_result[idx] = 0
+
+    fig, ax = plt.subplots()
+
+    sns.heatmap(result, ax=ax, cmap=plt.cm.Blues)
+
+    ax.set_title("k-means heat map (seed={})".format(seed))
+    ax.set_xlabel("label")
+    ax.set_ylabel("cluster")
+
+    ax.set_xticks(np.arange(result.shape[0]) + .5, minor=False)
+    ax.set_yticks(np.arange(result.shape[1]) + .5, minor=False)
+
+    # ax.invert_yaxis()
+    # ax.xaxis.tick_top()
+
+    ax.set_xticklabels(range(10), minor=False)
+    ax.set_yticklabels([""]*10, minor=False)
+
+    plt.savefig(save_dir + '/heatmap.png')
 
 
-if __name__ == "__main__":
-    seed = 1
+def k_means_pp_init(data):
+    center_idx = [np.random.randint(0, len(data))]
+
+    for _ in range(1, 10):
+        _sqr_distance = np.array([np.sum(((data - data[idx]) ** 2), axis=1) for idx in center_idx])
+        nearest_sqr_distances = np.min(_sqr_distance.transpose(), axis=1)
+        sum_sqr_distances = np.sum(nearest_sqr_distances)
+        center_idx.append(np.random.choice(len(data), size=1, p=nearest_sqr_distances/sum_sqr_distances))
+
+    center_idx = np.array(center_idx)
+    centers = data[center_idx]
+    return centers
+
+
+def k_means(data, seed=1, save_dir=""):
     np.random.seed(seed)
-    save_dir = os.path.dirname(os.path.abspath(__file__)) + "/data/seed_" + str(seed)
+    save_name = save_dir + "/center.pkl"
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
+    if os.path.exists(save_name):
+        with open(save_name, 'rb') as f:
+            centers = pickle.load(f)
+        return centers
+
+    # centers = np.random.rand(10, 784)
+    centers = k_means_pp_init(data)
+
+    step = 0
+    while step < 80:
+        print("step: {}".format(step))
+        old_centers = centers
+        masks = e_step(data, centers)
+        centers = m_step(data, masks)
+        done = np.allclose(old_centers, centers)
+        if step % 10 == 0 or done:
+            for i, center in enumerate(centers):
+                image = np.uint8(center.reshape((28, 28)) * 255)
+                Image.fromarray(image).save(save_dir + '/img_{}_{}.jpg'.format(step, i))
+        if done:
+            print("Clustering is converged")
+            break
+        step += 1
+
+    with open(save_name, 'wb') as f:
+        print("make pickle file")
+        pickle.dump(centers, f, -1)
+
+    return centers
+
+
+if __name__ == "__main__":
     _data = load()
     train_images = _data["train_images"]/255.
     train_labels = _data["train_labels"]
 
-    # _centers = np.random.rand(10, 784)
-    _center_idx = [np.random.randint(0, len(train_images))]
+    _scores_df = pd.DataFrame()
+    for _seed in range(100):
+        _file_path = os.path.dirname(os.path.abspath(__file__))
+        _save_dir = _file_path + "/data/seed_" + str(_seed)
 
-    for _ in range(1, 10):
-        _sqr_distance = np.array([np.sum(((train_images - train_images[idx]) ** 2), axis=1) for idx in _center_idx])
-        nearest_sqr_distances = np.min(_sqr_distance.transpose(), axis=1)
-        sum_sqr_distances = np.sum(nearest_sqr_distances)
-        print(nearest_sqr_distances.shape)
-        print(sum_sqr_distances.shape)
-        _center_idx.append(np.random.choice(len(train_images), size=1, p=nearest_sqr_distances/sum_sqr_distances))
-
-    _center_idx = np.array(_center_idx)
-    print("center_idx:\n", _center_idx)
-    print("center_label\n", train_labels[_center_idx])
-    _centers = train_images[_center_idx]
-
-    step = 0
-    while step < 100:
-        print("step: {}".format(step))
-        _old_centers = _centers
-        _masks = e_step(train_images, _centers)
-        _centers = m_step(train_images, _masks)
-        done = np.allclose(_old_centers, _centers)
-        if step % 10 == 0 or done:
-            print("Each cluster:")
-            accuracy(train_labels, _masks)
-            for i, _center in enumerate(_centers):
-                image = np.uint8(_center.reshape((28, 28)) * 255)
-                Image.fromarray(image).save(save_dir + '/img_{}_{}.jpg'.format(step, i))
-        if done:
-            print("Clustering converged")
-            break
-        step += 1
+        _centers = k_means(train_images, seed=_seed, save_dir=_save_dir)
+        draw_heatmap(train_images, train_labels, _centers, seed=_seed, save_dir=_save_dir)
+        _score = entropy_score(train_images, train_labels, _centers)
+        _score_df = pd.DataFrame({
+            'seed': [_seed],
+            'score': [_score]
+        })
+        _scores_df = _scores_df.append(_score_df)
+        _scores_df.to_csv(_file_path + "/data/score.csv")
